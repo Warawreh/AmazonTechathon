444a5c680de978f6fc339ec5f772a4c5a3a8e5b7
Bahaa55 <bhaaabualrub2001@gmail.com>
2020-09-14 18:21:04 +0300

    add commentPage (not ready yet)

---
+++ CommentPage.php
@@ -0,0 +1,235 @@
+<?php require 'header.php'; 
+ require 'method.php';
+  ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/commentStyle.css" />
+        <link rel="stylesheet" href="css/header.css" />
+        <link rel="stylesheet" type="text/css" media="only screen and (max-device-width: 480px)" href="css/phoneStyle.css" />
+        <link rel="stylesheet" type="text/css" media="only screen and (max-device-width: 801px)" href="css/tablet.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+        <title>Discussion Page</title>
+    </head>
+    <script>
+    
+        $(document).ready(function() {
+            $('#adding').submit(function(e){
+                e.preventDefault();
+                var n = $('#txt').val();
+                var u ="";
+                
+        
+// see if someone logged in
+                <?php if (isset($_COOKIE["uname"])){
+                
+                    $x=$_COOKIE["uname"]; ?>
+                                u= '<?php echo $x; ?>';
+                                console.log(u);
+                <?php } ?>
+                    
+                    
+// get the pageid for the comment            
+                    var p=0;
+                    <?php
+                    if(isset($_GET['pid'])&&$_GET['pid']!=""){
+                    
+                        $xx=$_GET['pid']; ?>
+                    p ='<?php echo $xx; ?>';
+                    console.log(p);
+                    <?php } ?>
+                        
+// use ajax to add the comment 
+                $.ajax({
+                    type:"POST",
+                    url:"addComment.php",
+                    data: "subject="+n+"&p=0"+"&name="+u+"&pageid="+p,
+                    success: function(){
+//empty the textarea and disable the Add button
+                    $("#txt").val('');
+                    $('.mainB').prop('disabled',true);
+                    
+//Update the comments by ajax call
+                    $.ajax({
+                        type: "GET",
+                        url: "loadAllComments.php?pid="+p,
+                        dataType: "html",
+                        success: function (response) {
+                            $("#CommentsBlock").html(response);
+                            $(".ANY").hide();
+                        }
+                    });
+                }
+                });
+                
+            });
+        });
+    
+    
+    
+    </script>
+    <body>
+    
+
+    <hr>
+    <div class="down">
+        <div  style="background-color: white;">
+            
+                <div >
+                    <h1 style="text-align: center">Comments</h1>
+                </div>
+
+
+                <div class="ncomment mn">
+                    <br>
+                    <div class="uside">
+                    <laber style="font-weight: bold;">UserName :replied to alaa </laber>
+                    <div class="w">
+                    dd/mm/yyyy hh:mm:ssa
+                </div>
+                </div>
+                    
+                    <div class="mainc">
+                        welcom to the arabic site 
+                    </div>
+                    <br>
+                    <div class="rightc">
+                    
+                    <i class="fas fa-thumbs-up" style="float:right;margin-left: 3%">()</i>
+                    <i class="fas fa-reply" style="float:right;margin-left: 3%"></i>
+                    <i class="fas fa-trash-alt" style="float:right;margin-left: 3%"></i>
+                    <i class="far fa-edit" style="float:right;margin-left: 3%"></i>
+                </div>
+                </div>
+                
+                <div class="ncomment mn">
+                    <br>
+                    <div class="uside">
+                    <laber style="font-weight: bold;">UserName : </laber>
+                    <div  class="w">
+                    dd/mm/yyyy hh:mm:ssa
+                </div>
+                </div>
+                    
+                    <div class="mainc">
+                        welcom to the arabic site 
+                    </div>
+                    <br>
+                    <div class="rightc">
+                    
+                    <i class="fas fa-thumbs-up" style="float:right;margin-left: 3%">()</i>
+                    <i class="fas fa-reply" style="float:right;margin-left: 3%"></i>
+                    <i class="fas fa-trash-alt" style="float:right;margin-left: 3%"></i>
+                    <i class="far fa-edit" style="float:right;margin-left: 3%"></i>
+                </div>
+                </div>
+
+                
+                <div class="ncomment ed">
+                    <br>
+                    <div class="uside">
+                    <laber style="font-weight: bold;">Edit your comment : </laber>
+                    
+                </div>
+                    
+                    <div class="mainc">
+                        <textarea class="earea"></textarea>
+                    </div>
+                    
+                    <div class="rightc">
+                    
+                        <button class="ebtn">Submit</button>
+                </div>
+                </div>
+                
+                <div  class="rcomment re">
+                    <br>
+                    <div class="uside">
+                    <laber style="font-weight: bold;">UserName : </laber>
+                    <div class="w">
+                    dd/mm/yyyy hh:mm:ssa
+                </div>
+                </div>
+                    
+                    <div class="mainc">
+                        welcom to the arabic site 
+                    </div>
+                    
+                    <div class="rightc">
+                    
+                    <i class="fas fa-thumbs-up" style="float:right;margin-left: 3%">(5)</i>
+                    <i class="fas fa-reply" style="float:right;margin-left: 3%"></i>
+                    <i class="fas fa-trash-alt" style="float:right;margin-left: 3%"></i>
+                    <i class="far fa-edit" style="float:right;margin-left: 3%"></i>
+                </div>
+                </div>
+
+                <div class="rcomment re">
+                    <br>
+                    <div class="uside">
+                    <laber style="font-weight: bold;">Replying to Username : </laber>
+                    
+                </div>
+                    
+                    <div class="mainc">
+                        <textarea class="earea"></textarea>
+                    </div>
+                    
+                    <div class="rightc">
+                    
+                        <button class="ebtn">Submit</button>
+                </div>
+                </div>
+                       
+                    
+                <div class="ncomment re">
+                    <br>
+                    <div class="uside">
+                    <laber style="font-weight: bold;">Replying to Username : </laber>
+                    
+                </div>
+                    
+                    <div class="mainc">
+                        <textarea class="earea"></textarea>
+                    </div>
+                    
+                    <div class="rightc">
+                    
+                        <button class="ebtn">Submit</button>
+                </div>
+                </div>
+
+                        
+
+
+
+                   
+
+
+                       
+                            
+            
+        </div>
+    </div>
+    <hr>
+
+    <!-- THe submit form goes here -->
+    <div class="sb">
+                    <form method="POST">
+                        <label>
+                            <h2 style="text-align:center">Add Comment</h2>
+                            <textarea class="tsb"></textarea>
+                            <button class="bsb">Add</button>
+                        </label>
+                    </form>
+                
+    </div>
+        
+        
+        
+    </body>
+    
+    
+    
+    
+</html>
---
+++ method.php
@@ -0,0 +1,13 @@
+    
+    
+    
+      
+    
+        
+        <link rel="stylesheet" href="css/cmp.css" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css2?family=Tajawal&display=swap" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="img/web_icon.jpg">
+	<meta charset="UTF-8">
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery.js"></script>
