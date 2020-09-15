49e61f90b4c71d7c6827cd0f8ebdbf5bbc7d2643
Warawreh <70857825+Warawreh@users.noreply.github.com>
2020-09-15 18:11:21 +0300

    Bug fixing

--- profile.php
+++ profile.php
@@ -91,7 +91,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+
   <link rel="stylesheet" href="css/style.css"        type="text/css">
 </head>
 <body>
@@ -204,5 +204,7 @@
   }
 
 </script>
-
+<?php
+require 'footer.html';
+?>
 </html>
